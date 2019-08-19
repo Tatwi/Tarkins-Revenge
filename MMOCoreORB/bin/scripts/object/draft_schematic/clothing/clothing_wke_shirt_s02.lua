8edd5237f528f2442985e875757f57c7509cc112
stacy19201325 <stacy_19201325@hotmail.com>
2019-08-19 07:03:23 -0500

    [Fixed] Wookiee BE tissue slots

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_shirt_s02.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_shirt_s02.lua
@@ -62,12 +62,12 @@ object_draft_schematic_clothing_clothing_wke_shirt_s02 = object_draft_schematic_
    customizationStringNames = {},
    customizationDefaults = {},
 
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"heavy_shell", "binding_and_weatherproofing"},
-   ingredientSlotType = {0, 0},
-   resourceTypes = {"hide", "petrochem_inert_polymer"},
-   resourceQuantities = {35, 5},
-   contribution = {100, 100},
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTitleNames = {"heavy_shell", "binding_and_weatherproofing", "synthetic_cloth", "reinforced_fiber_panels"},
+   ingredientSlotType = {0, 0, 1, 1},
+   resourceTypes = {"hide", "petrochem_inert_polymer", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {35, 5, 1, 1},
+   contribution = {100, 100, 100, 100},
 
 
    targetTemplate = "object/tangible/wearables/wookiee/wke_shirt_s02.iff",
