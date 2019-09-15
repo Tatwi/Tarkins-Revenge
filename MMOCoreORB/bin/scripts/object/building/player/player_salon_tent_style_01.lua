9ff413fcd9cb1c1d50dd3052446984ffac98a970
stacy19201325 <stacy_19201325@hotmail.com>
2019-09-15 09:40:25 -0500

    [Added] NGE signs to civic buildings

--- MMOCoreORB/bin/scripts/object/building/player/player_salon_tent_style_01.lua
+++ MMOCoreORB/bin/scripts/object/building/player/player_salon_tent_style_01.lua
@@ -58,7 +58,13 @@ object_building_player_player_salon_tent_style_01 = object_building_player_share
 			{templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign1"},
 			{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign2"},
 			{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign3"},
-			{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign4"},			
+			{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign4"},
+		{templateFile = "object/tangible/tcg/series3/house_sign_tcg_s02.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = -0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_sign_tcg_s02"},
+		{templateFile = "object/tangible/tcg/series5/house_sign_tcg_standing.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_sign_tcg_standing"},
+		{templateFile = "object/tangible/sign/player/efol_standing_sign_01.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = -0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:efol_sign_standing"},
+		{templateFile = "object/tangible/sign/player/shop_sign_halloween_sign.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = -0.707107, oz = 0, ow = -0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@static_item_n:item_special_sign_halloween_standing_sign"},
+		{templateFile = "object/tangible/sign/player/imperial_empire_day_2009_sign_standing.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = -0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:empire_day_imp_standing_sign"},
+		{templateFile = "object/tangible/sign/player/rebel_remembrance_day_2009_sign_standing.iff", x = 3.5, z = 0, y = 3.5, ox = 0, oy = -0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:empire_day_reb_standing_sign"},				
 	},		
 }
 
