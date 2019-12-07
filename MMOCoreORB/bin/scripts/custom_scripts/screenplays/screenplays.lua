5c615c24a4916b1d47dc9ad8354b3007ca89abaf
stacy19201325 <stacy_19201325@hotmail.com>
2019-12-07 11:04:32 -0600

    [Moved] tarkin_lib to custom scripts

--- MMOCoreORB/bin/scripts/custom_scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/custom_scripts/screenplays/screenplays.lua
@@ -3,5 +3,7 @@
 
 includeFile("../custom_scripts/screenplays/tools/epiphany_schematic_table.lua")
 includeFile("../custom_scripts/screenplays/tools/incubator_table.lua")
+includeFile("../custom_scripts/screenplays/tools/tarkin_lib.lua")
 includeFile("../custom_scripts/screenplays/tools/reprocessor_table.lua")
 includeFile("../custom_scripts/screenplays/tools/Writeables.lua")
+
